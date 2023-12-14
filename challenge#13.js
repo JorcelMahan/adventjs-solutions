81ab8300975e48c40b812c3298e34bff849d537d
Joma Work <johanmfc11@gmail.com>
2023-12-14 14:43:41 -0400

    Add maxGifts function to calculate maximum gifts in challenge#14.js

--- challenge#13.js
+++ challenge#13.js
@@ -38,7 +38,5 @@ function calculateTime(deliveries) {
 
     const formattedTime = `${sign}${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
     return formattedTime;
-
-
 }
 
---
+++ challenge#14.js
@@ -0,0 +1,19 @@
+// console.log(maxGifts([2, 4, 2])) // 4 (4)
+// console.log(maxGifts([5, 1, 1, 5])) // 10 (5 + 5)
+// console.log(maxGifts([4, 1, 1, 4, 2, 1])) // 9 (4 + 4 + 1)
+console.log(maxGifts([1, 3, 1, 3, 100]))// 103 (3 + 100)
+
+
+
+function maxGifts(houses) {
+    let prevMax = 0;
+    let currentMax = 0;
+
+    for (let i = 0; i < houses.length; i++) {
+        let newMax = Math.max(currentMax, prevMax + houses[i]);
+        prevMax = currentMax;
+        currentMax = newMax;
+    }
+
+    return currentMax;
+}
